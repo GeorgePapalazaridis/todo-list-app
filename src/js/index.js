68c857956426a84c0b17f36e39dc717f9c5b76b2
George Papalazaridis <georgepapalazaridis@gmail.com>
2024-11-26 04:16:50 +0200

    Implement theme toggle functionality and update styles with CSS variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 import "./TodoList.js";
 import { loadDialog } from "./PopupDialog.js";
+import { initializeTheme } from "./theme.js";
 
 loadDialog();
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeTheme();
+});
